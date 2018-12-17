8e326f3e39dd1f00e2d2e65395d9f5f086da90fe
lauchlanc <lauchlanchisholm@gmail.com>
2018-12-17 02:14:20 +0000

    Incorporating flow and jest

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root: ?Element = document.getElementById('root');
+
+if(root){
+  ReactDOM.render(<App foo={43} />, root);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
