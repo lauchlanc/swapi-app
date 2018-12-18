1213ed30f3b0cdca1a4fd7a58b6b6d0f005d4b24
Lauchlan Chisholm <lauchlanchisholm@Lauchlans-MacBook-Pro.local>
2018-12-18 11:30:17 -0400

    Updates

---
+++ precache-manifest.a1db1da3229f7ebfe70cba9e9ab3a0ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "16536c2505b895c7d6b9c8c31932ead5",
+    "url": "/lauchlanc/swapi-app/static/media/space.16536c25.png"
+  },
+  {
+    "revision": "2e4838bd09b89fab57de95b5b8afe711",
+    "url": "/lauchlanc/swapi-app/static/media/death-star.2e4838bd.svg"
+  },
+  {
+    "revision": "894e50ea00b6b1602fa8",
+    "url": "/lauchlanc/swapi-app/static/js/runtime~main.894e50ea.js"
+  },
+  {
+    "revision": "a8a30d299896190b1b61",
+    "url": "/lauchlanc/swapi-app/static/js/main.a8a30d29.chunk.js"
+  },
+  {
+    "revision": "a4c76abc2836c0094149",
+    "url": "/lauchlanc/swapi-app/static/js/1.a4c76abc.chunk.js"
+  },
+  {
+    "revision": "a8a30d299896190b1b61",
+    "url": "/lauchlanc/swapi-app/static/css/main.5bee609e.chunk.css"
+  },
+  {
+    "revision": "a4c76abc2836c0094149",
+    "url": "/lauchlanc/swapi-app/static/css/1.bc319cde.chunk.css"
+  },
+  {
+    "revision": "ac59ee257906965f7c64842b0b69d2de",
+    "url": "/lauchlanc/swapi-app/index.html"
+  }
+];
