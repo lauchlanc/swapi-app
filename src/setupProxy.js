a072be5be374176509ca0a53ca1c29b575aa3218
Lauchlan Chisholm <lauchlanchisholm@Lauchlans-MacBook-Pro.local>
2018-12-18 20:43:07 -0400

    Reverting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,7 @@
 const proxy = require('http-proxy-middleware')
 
-const PORT: number = process.env.PORT != null
-  ? parseInt(process.env.PORT, 10)
-  : 8080;
-
 module.exports = function(app) {
-  app.use(proxy('/api', { target: `http://localhost:${PORT}/` }))
+  app.use(proxy('/api', { target: 'http://localhost:8080/' }))
 }
 
 // more info https://github.com/facebook/create-react-app/issues/5103
