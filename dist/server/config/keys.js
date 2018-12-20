06cc2ad3b123d883318fdab3f7ccde2e821dffcc
Lauchlan Chisholm <lauchlanchisholm@Lauchlans-MacBook-Pro.local>
2018-12-20 11:30:45 -0400

    Updating gitignore

--- dist/server/config/keys.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = {
-  mongoURI: 'mongodb+srv://visitor:MoneyStar4@sandbox-1hmco.mongodb.net/swapi?retryWrites=true'
-};
--- dist/setupProxy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var proxy = require('http-proxy-middleware');
-
-module.exports = function (app) {
-  app.use(proxy('/api', { target: 'http://localhost:8080/' }));
-};
-
-// more info https://github.com/facebook/create-react-app/issues/5103
